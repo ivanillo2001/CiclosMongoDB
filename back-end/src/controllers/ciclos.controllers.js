b42c4dbb28fa6493fe2f4102fe09566bdfb7bd93
ivanillo2001 <ivanolo3000@gmail.com>
2024-02-08 13:43:09 +0100

    Primer commit Codigo

--- back-end/src/controllers/ciclos.controllers.js
+++ back-end/src/controllers/ciclos.controllers.js
@@ -1,5 +1,6 @@
-import conexion from "../mysql_conector.js";
-
+// import conexion from "../mysql_conector.js";
+import conexionBD from "../../mongodb_conector.js";
+import { ObjectId } from "mongodb";
 /**
  * @description Funcion encargada de hacer la petición a la bbdd para
  * obtener los cursos.
@@ -8,9 +9,11 @@ import conexion from "../mysql_conector.js";
  */
 export const mostrarCursos = async (req, res) => {
   try {
-    const [result] = await conexion.query("Select * from cursos"); 
-    console.log(result);
-    res.status(200).json(result);
+    const database = await conexionBD();
+    console.log(database);
+    const collection = database.collection("cursos")
+    const result = await collection.find({}).toArray()
+    res.status(200).json(result)
   } catch (error) {
     console.error(error);
     res.status(500).json({
@@ -25,56 +28,56 @@ export const mostrarCursos = async (req, res) => {
  * @param {*} req 
  * @param {*} res 
  */
-export const mostrarModulos = async (req, res) => {
-  try {
-    const { id } = req.params; //para extraer el atributo id de params
-    const [result] = await conexion.query(
-      "SELECT * FROM modulos where idCurso=? order by descripcion",
-      [id]
-    );
-    res.status(200).json(result);
-  } catch (error) {
-    res.status(500).json({
-      message: "Error en el servidor",
-    });
-  }
-};
+// export const mostrarModulos = async (req, res) => {
+//   try {
+//     const { id } = req.params; //para extraer el atributo id de params
+//     const [result] = await conexion.query(
+//       "SELECT * FROM modulos where idCurso=? order by descripcion",
+//       [id]
+//     );
+//     res.status(200).json(result);
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "Error en el servidor",
+//     });
+//   }
+// };
 
-/**
- * @description Función encargada de hacer la petición a la bbdd para mostrar los alumnos 
- * que tiene un curso. Éste se pasa como parámetro (req)
- * @param {*} req 
- * @param {*} res 
- */
-export const mostrarAlumnos = async (req, res) => {
-  //argumentos de peticion y respuesta
-  try {
-    const { id } = req.params; //para extraer el atributo id de params
-    const [result] = await conexion.query(
-      "SELECT * FROM alumnos where idCurso=? order by apellidosNombre",
-      [id]
-    );
-    res.status(200).json(result);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: "Error en el servidor",
-    });
-  }
-};
-export const grabarDatos = async (req,res)=>{
-  try {
-    console.log("Pasa1");
-    console.log(req.query);
-    const {idCurso, idModulo,idAlumno,calificacion}=req.body;
-    console.log("Pasa 2");
-    console.log(req.body);
-    const [result]= await conexion.query("INSERT INTO calificaciones (idCalificaciones, idCurso, idModulo, idAlumno, calificacion) VALUES (NULL,?,?,?,?)",[idCurso,idModulo,idAlumno,calificacion])
-    res.status(201).json({ idCurso: result.insertId });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: "Error en el servidor",
-    });
-  }
-}
+// /**
+//  * @description Función encargada de hacer la petición a la bbdd para mostrar los alumnos 
+//  * que tiene un curso. Éste se pasa como parámetro (req)
+//  * @param {*} req 
+//  * @param {*} res 
+//  */
+// export const mostrarAlumnos = async (req, res) => {
+//   //argumentos de peticion y respuesta
+//   try {
+//     const { id } = req.params; //para extraer el atributo id de params
+//     const [result] = await conexion.query(
+//       "SELECT * FROM alumnos where idCurso=? order by apellidosNombre",
+//       [id]
+//     );
+//     res.status(200).json(result);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({
+//       message: "Error en el servidor",
+//     });
+//   }
+// };
+// export const grabarDatos = async (req,res)=>{
+//   try {
+//     console.log("Pasa1");
+//     console.log(req.query);
+//     const {idCurso, idModulo,idAlumno,calificacion}=req.body;
+//     console.log("Pasa 2");
+//     console.log(req.body);
+//     const [result]= await conexion.query("INSERT INTO calificaciones (idCalificaciones, idCurso, idModulo, idAlumno, calificacion) VALUES (NULL,?,?,?,?)",[idCurso,idModulo,idAlumno,calificacion])
+//     res.status(201).json({ idCurso: result.insertId });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({
+//       message: "Error en el servidor",
+//     });
+//   }
+// }
--- back-end/src/routes/ciclos.routes.js
+++ back-end/src/routes/ciclos.routes.js
@@ -3,16 +3,16 @@
  * Importaciones necesarias:
  */
 import { Router } from "express";
-import {mostrarCursos, mostrarModulos, mostrarAlumnos,grabarDatos} from "../controllers/ciclos.controllers.js";//con esto importamos las funciones desde controller
+import {mostrarCursos} from "../controllers/ciclos.controllers.js";//con esto importamos las funciones desde controller
 
 const router = Router(); //declaración del router
 
 router.get("/cursos",mostrarCursos);//dirección de la funcion de mostrar cursos
 
-router.get("/modulos/:id",mostrarModulos);//dirección de la funcion de mostrarModulos. Se pasa el idCurso como parametro
-
-router.get("/alumnos/:id",mostrarAlumnos)//dirección de la funcion de mostrarAlumnos. Se pasa el idCurso como parametro
-
-router.post("/notas/",grabarDatos)//dirección de la funcion de mostrarAlumnos. Se pasa el idCurso, idModulo, idAlumno y calificacion como parametros
+// router.get("/modulos/:id",mostrarModulos);//dirección de la funcion de mostrarModulos. Se pasa el idCurso como parametro
+// 
+// router.get("/alumnos/:id",mostrarAlumnos)//dirección de la funcion de mostrarAlumnos. Se pasa el idCurso como parametro
+// 
+// router.post("/notas/",grabarDatos)//dirección de la funcion de mostrarAlumnos. Se pasa el idCurso, idModulo, idAlumno y calificacion como parametros
 
 export default router;//lo exportamos.
